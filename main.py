39ab40c01fafcb1714e5037574ece5d48ef22e0b
Teo <75574673+sunsakis@users.noreply.github.com>
2024-02-24 22:46:36 +0200

    backup

--- main.py
+++ main.py
@@ -35,21 +35,24 @@ BOT_USERNAME: Final = '@YourBabushkaBot'
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
 
 biker_ids = load_chat_ids()
+rider_id = None
 
 async def start(update: Update, context: CallbackContext) -> None:
-    location_keyboard = KeyboardButton(text="Share Location", request_location=True)
+    location_keyboard = KeyboardButton(text="Share Current Location", request_location=True)
     custom_keyboard = [[location_keyboard]]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     await update.message.reply_text(
-        'Please share your location to start. Make sure precise location sharing is enabled.',
+        'Please share your location to start. Make sure your phone allows precise location sharing when using Telegram.',
         reply_markup=reply_markup
     )
-    context.user_data['state'] = 'LOCATION'
+    context.user_data['state'] = 'USER_LOCATION'
     
 async def handle_location(update: Update, context: CallbackContext) -> None:
-    if context.user_data['state'] == 'LOCATION':
+    print(context.user_data['state'])
+    print(update.message.location)
+    if context.user_data['state'] == 'USER_LOCATION':
         user_location = update.message.location
-        context.user_data['LOCATION'] = user_location
+        context.user_data['USER_LOCATION'] = user_location
         await update.message.reply_text(
             'Thanks for sharing your location. Now, hit the paperclip and choose the location you want to get to.'
         )
@@ -61,25 +64,60 @@ async def handle_location(update: Update, context: CallbackContext) -> None:
         await update.message.reply_text('How many euros do you want to pay for the ride? Write in numbers only, for example 5 or 10.')
         context.user_data['state'] = 'AWAITING_PRICE'
 
+    elif context.user_data['state'] == 'BIKER_LOCATION':
+        biker_location = update.message.location
+        context.user_data['BIKER_LOCATION'] = biker_location
+        await update.message.reply_text(
+            'Shared your live location. Now go get your rider!'
+        )
+        # Send the biker's live location to the rider
+        await context.bot.sendLocation(
+            chat_id=rider_id,
+            latitude=biker_location.latitude,
+            longitude=biker_location.longitude,
+            live_period=86400,
+            horizontal_accuracy=0
+        )
+
+        message_id = update.message.message_id
+        context.user_data['message_id'] = message_id
+
+        await context.bot.send_message(
+            chat_id=rider_id,
+            text='Your biker is on the way!'
+        )
+
+        context.user_data['state'] = 'ON_THE_WAY'
+
+    elif context.user_data['state'] == 'ON_THE_WAY':
+        print(f'new location: {update.message.location}')
+        biker_location = update.message.location
+        await context.bot.editMessageLiveLocation(
+            latitude=context.user_data['BIKER_LOCATION'].latitude,
+            longitude=context.user_data['BIKER_LOCATION'].longitude,
+            chat_id=rider_id,
+            message_id=context.user_data['message_id']
+        )
+
+
 async def handle_price(update: Update, context: CallbackContext) -> None:
 
     # Get driving route from Google Maps Directions API
     route = requests.get(
             'https://maps.googleapis.com/maps/api/directions/json',
             params={
-                'origin': f"{context.user_data['LOCATION'].latitude},{context.user_data['LOCATION'].longitude}",
+                'origin': f"{context.user_data['USER_LOCATION'].latitude},{context.user_data['USER_LOCATION'].longitude}",
                 'destination': f"{context.user_data['DESTINATION'].latitude}, {context.user_data['DESTINATION'].longitude}",
                 'key': GOOGLE_MAPS_API_KEY,
             },
             )
     
-    location_address = gmaps.reverse_geocode((context.user_data['LOCATION'].latitude, context.user_data['LOCATION'].longitude))
+    location_address = gmaps.reverse_geocode((context.user_data['USER_LOCATION'].latitude, context.user_data['USER_LOCATION'].longitude))
     destination_address = gmaps.reverse_geocode((context.user_data['DESTINATION'].latitude, context.user_data['DESTINATION'].longitude))
     # Clean geocode to only include formatted address
     location_address = ','.join(location_address[0]['formatted_address'].split(',')[:3]).strip()
     destination_address = ','.join(destination_address[0]['formatted_address'].split(',')[:3]).strip()
-    print(gmaps.reverse_geocode((context.user_data['DESTINATION'].latitude, context.user_data['DESTINATION'].longitude))[0]['formatted_address'])
-    
+
     # Check if the route request was successful
     if route.status_code == 200:
         directions = route.json()
@@ -110,6 +148,8 @@ async def handle_price(update: Update, context: CallbackContext) -> None:
     if context.user_data['state'] == 'AWAITING_PRICE':
         price = update.message.text
         if price and price.isdigit():
+            global rider_id
+            rider_id = update.message.from_user.id
             context.user_data['PRICE'] = int(price)
             context.user_data['state'] = 'PRICE'
             await update.message.reply_text(f'Bikers have been notified of a €{price} ride! The higher the price, the faster they come!')
@@ -138,23 +178,13 @@ async def handle_price(update: Update, context: CallbackContext) -> None:
 async def accept_request(update: Update, context: CallbackContext):
     # Get the callback query
     query = update.callback_query
+    # Answer the callback query
+    await query.answer()
+    await query.edit_message_text('Send Your Current Location to accept the ride request. You will get paid when you reach the pick-up location.')
+    # Ask the biker to share location
 
-    # Get the biker's location (replace with actual location)
+    context.user_data['state'] = 'BIKER_LOCATION'
 
-    # Send a message to the user
-    context.bot.send_message(chat_id=query.message.chat_id,
-                             text="A biker has accepted your pick-up request.")
-    
-    # Ask the biker to share their live location
-    context.bot.send_message(chat_id=query.from_user.id,
-                             text="Please share your live location to start the ride.")
-
-    # Share the biker's live location with the user
-    context.bot.send_location(chat_id=query.message.chat_id,
-                              latitude=update.message.location.latitude,
-                              longitude=update.message.location.longitude,
-                              live_period=86400)  # Live for 24 hours
-        
 async def join(update: Update, context: CallbackContext) -> None:
     context.user_data['state'] = 'CITY'
     chat_id = update.effective_chat.id
@@ -312,7 +342,7 @@ if __name__ == '__main__':
     app.add_handler(MessageHandler(filters.TEXT, handle_price))
     app.add_handler(MessageHandler(filters.TEXT, handle_city))
     app.add_handler(CommandHandler('help', help_command))
-    app.add_handler(CommandHandler('saskaita', invoice_command))
+    app.add_handler(CallbackQueryHandler(accept_request))
     # app.add_handler(PreCheckoutQueryHandler(precheckout_callback))
 
     # Messages
